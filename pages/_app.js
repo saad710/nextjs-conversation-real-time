43e86a5fcabbf5a25a27f50344c8bb7c1abed39e
Md. Ifrat Hasnat <ifrathasnat2@gmail.com>
2022-01-03 10:07:11 +0600

    all-issue-solved-after-using-redux-thunk

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import LoginProvider from '../context/AuthContext/LoginProvider'
 import '../styles/globals.css'
 import { Provider } from "react-redux";
 import { configureStore } from '../redux/store';
@@ -6,9 +5,7 @@ import { configureStore } from '../redux/store';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={configureStore()}>
-    <LoginProvider>
       <Component {...pageProps} />
-    </LoginProvider>
     </Provider>
   )
 }
